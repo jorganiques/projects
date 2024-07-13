dda34278ebbbba2051deef09f76dd603fcd23307
joanna abinuman <joannaabinuman@joannas-MacBook-Pro.local>
2024-07-13 22:25:40 +0800

    Project: Vanilla JavaScript App

--- factumverify/script.js
+++ factumverify/script.js
@@ -1,125 +1,98 @@
-const newsApiKey = 'd33534e32a2e4e91ab519acfb9ab8b42'; // Setup value of apiKey with your actual News API key
-const meaningCloudApiKey = '3cad836143b352a37661657d43213759'; // Replace with your actual MeaningCloud API key
+// Object to store API keys
+const apiKeys = {
+    newsApiKey: 'd33534e32a2e4e91ab519acfb9ab8b42', // Setup value of apiKey with your actual News API key
+    meaningCloudApiKey: '3cad836143b352a37661657d43213759' // Replace with your actual MeaningCloud API key
+};
 
-function clearPreviousResults() {
-    const resultsPreviousSection = document.getElementById('results');
-    resultsPreviousSection.innerHTML = '';
+// Function to show or hide the progress bar
+function toggleProgressBar(display, elementId = 'mainProgressBar') {
+    const progressBar = document.getElementById(elementId);
+    if (progressBar) { // Check if progressBar element exists
+        progressBar.style.display = display ? 'block' : 'none';
+    } else {
+        console.warn(`Progress bar element with ID ${elementId} not found.`);
+    }
 }
 
-document.getElementById('factCheckForm').addEventListener('reset', function(event) {
-    // Clear previous results.
-    clearPreviousResults();
-});
+// Function to clear previous results from the results section
+function clearPreviousResults() {
+    document.getElementById('results').innerHTML = '';
+}
 
-document.getElementById('factCheckForm').addEventListener('submit', async function(event) {
-    // Prevent default form submission behavior.
-    event.preventDefault();
-    
-    // Get the query from the input field.
-    const query = document.getElementById('query').value;
-    
-    // Get the selected sort option.
-    const sortOption = document.querySelector('input[name="sort"]:checked').value;
+// Function to display an error message in the results section
+function displayError(message) {
+    document.getElementById('results').innerHTML = `<p class="error">${message}</p>`;
+}
 
-    // Get the selected language.
-    const language = document.getElementById('language').value;
-    
-    // Construct the API URL with the query, sort option, language, and API key.
-    const apiUrl = `https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&sortBy=${sortOption}&language=${language}&apiKey=${newsApiKey}`;
+// Function to create and return a result element based on the article data
+function createResultElement(article, index) {
+    const resultElement = document.createElement('div');
+    resultElement.classList.add('result');
+    resultElement.innerHTML = `
+        <h2>${article.title}</h2>
+        <p><strong>Source:</strong> ${article.source.name}</p>
+        <p><strong>Author:</strong> ${article.author}</p>
+        <p><strong>Published At:</strong> ${new Date(article.publishedAt).toLocaleDateString()}</p>
+        <p>${article.description}</p>
+        <img src="${article.urlToImage}" alt="news" class="centerToImage">
+        <p class="readMore"><a href="${article.url}" target="_blank">Read more</a></p>
+        <div class="summarize-container">
+            <button class="summarize" onclick="summarizeArticle('${article.url}', this, 'progressBar-${index}')">Summarize</button>
+        </div>
+        <div class="summary"></div>
+        <div id="progressBar-${index}" class="progress-bar" style="display: none;">
+            <div class="progress"></div>
+        </div>
+    `;
+    return resultElement;
+}
 
-    
-    // Construct the API URL with the query, sort option, and API key.
-    //const apiUrl = `https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&sortBy=${sortOption}&apiKey=${newsApiKey}`;
-
-    try {
-        // Fetch data from the API.
-        const response = await fetch(apiUrl);
-        
-        // If the response is not ok, throw an error.
-        if (!response.ok) throw new Error('Network response was not ok');
-        
-        // Parse the response data as JSON.
-        const data = await response.json();
-        
-        // Call displayResults with the parsed data.
-        displayResults(data);
-    } catch (error) {
-        // Call displayError with an error message.
-        displayError('Error fetching data. Please try again.');
-        console.error('Error fetching data:', error);
-    }
-});
-
+// Function to display the results in the results section
 function displayResults(data) {
-    // Clear previous results.
+    // Clear previous results
     clearPreviousResults();
 
-    // If no articles are found, display an error message and return.
+    // Check if there are no articles and display an error message if so
     if (!data.articles || data.articles.length === 0) {
         displayError('No results found. Please try a different query.');
         return;
     }
 
-    // For each article in data.articles:
+    // Get the results section element
     const resultsSection = document.getElementById('results');
-    data.articles.forEach(article => {
-
-    // Do not add if article is [Removed]    
-    if (article.title !== '[Removed]') {
-
-        // Create a result card with title, source, publication date, description, and a link to read more.
-        const resultElement = document.createElement('div');
-        resultElement.classList.add('result');
-        //console.log(article.url);
-        resultElement.innerHTML = `
-            <h2>${article.title}</h2>
-            <p><strong>Source:</strong> ${article.source.name}</p>
-            <p><strong>Author:</strong> ${article.author}</p>
-            <p><strong>Published At:</strong> ${new Date(article.publishedAt).toLocaleDateString()}</p>
-            <p>${article.description}</p>
-            <img src="${article.urlToImage}" alt="news" class="centerToImage">
-            <p class="readMore"><a href="${article.url}" target="_blank">Read more</a></p>
-            <div class="summarize-container">
-                <button class="summarize" onclick="summarizeArticle('${article.url}', this)">Summarize</button>
-            </div>
-            <div class="summary"></div>
-            <div class="progress-bar" style="display: none;">
-                <div class="progress"></div>
-            </div>
-        `;
-        // Append the result card to the results section.
-        resultsSection.appendChild(resultElement);
-    }  
+    
+    // Loop through each article and append the result element to the results section
+    data.articles.forEach((article, index) => {
+        if (article.title !== '[Removed]') {
+            resultsSection.appendChild(createResultElement(article, index));
+        }
     });
 }
 
-function displayError(message) {
-    // Display the error message in the results section.
-    const resultsSection = document.getElementById('results');
-    resultsSection.innerHTML = `<p class="error">${message}</p>`;
+// Function to fetch data from the given URL
+async function fetchData(url) {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error('Network response was not ok');
+    return response.json();
 }
 
-async function summarizeArticle(url, button) {
+// Function to summarize the article using the MeaningCloud API
+async function summarizeArticle(url, button, progressBarId) {
+    // Get the closest result element and the summary div
     const resultDiv = button.closest('.result');
     const summaryDiv = resultDiv.querySelector('.summary');
-    const progressBar = resultDiv.querySelector('.progress-bar');
-
-    progressBar.style.display = 'block';
+    
+    // Show the progress bar
+    toggleProgressBar(true, progressBarId);
 
     try {
-        const response = await fetch(`https://api.meaningcloud.com/summarization-1.0?key=${meaningCloudApiKey}&url=${encodeURIComponent(url)}&sentences=10`, {
-            method: 'POST'
-        });
+        // Construct the API URL for summarization
+        const apiUrl = `https://api.meaningcloud.com/summarization-1.0?key=${apiKeys.meaningCloudApiKey}&url=${encodeURIComponent(url)}&sentences=10`;
+        
+        // Fetch the summarization data
+        const data = await fetchData(apiUrl);
 
-        if (!response.ok) {
-            throw new Error(`API request failed with status ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        // Log the response to the console
-        console.log('API response:', data);
-
+        // Check the response status and display the summary or an error message
         if (data.status.code === '0') {
             summaryDiv.innerHTML = `<p>${data.summary}</p>`;
         } else {
@@ -129,6 +102,42 @@ async function summarizeArticle(url, button) {
         console.error('Error:', error);
         summaryDiv.innerHTML = `<p>There was an error processing your request. Please try again later.</p>`;
     } finally {
-        progressBar.style.display = 'none';
+        // Hide the progress bar
+        toggleProgressBar(false, progressBarId);
     }
 }
+
+// Event listener for the form reset event to clear previous results
+document.getElementById('factCheckForm').addEventListener('reset', clearPreviousResults);
+
+// Event listener for the form submit event to fetch and display news articles
+document.getElementById('factCheckForm').addEventListener('submit', async function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+    
+    // Show the progress bar
+    toggleProgressBar(true);
+
+    // Get the query, sort option, and language from the form inputs
+    const query = document.getElementById('query').value;
+    const sortOption = document.querySelector('input[name="sort"]:checked').value;
+    const language = document.getElementById('language').value;
+    
+    // Construct the API URL for fetching news articles
+    const apiUrl = `https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&sortBy=${sortOption}&language=${language}&apiKey=${apiKeys.newsApiKey}`;
+
+    try {
+        // Fetch the news articles data
+        const data = await fetchData(apiUrl);
+        
+        // Display the results
+        displayResults(data);
+    } catch (error) {
+        // Display an error message if there is an error fetching data
+        displayError('Error fetching data. Please try again.');
+        console.error('Error fetching data:', error);
+    } finally {
+        // Hide the progress bar
+        toggleProgressBar(false);
+    }
+});
